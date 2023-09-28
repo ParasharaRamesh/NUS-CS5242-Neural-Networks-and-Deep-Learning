e5eb7a5f4ff6d547c22f6a676b22b38e1b65ed8b
Parashara <parasharar@gmail.com>
2023-09-28 22:52:58 +0800

    Committing the code for q2.1 and q2.2

--- Assignment 2 (Autoencoders & CNNs)/Question-2_Autoencoders/CNN_autoencoder_model.py
+++ Assignment 2 (Autoencoders & CNNs)/Question-2_Autoencoders/CNN_autoencoder_model.py
@@ -1,10 +1,9 @@
 import torch
 import torch.nn as nn
-
-
+from torchinfo import summary
 class CNNAutoencoder(nn.Module):
     def __init__(self, lambda_value):
-        super(CNNAutoencoder, self).__init__()
+        super().__init__()
         self.lambda_value = lambda_value
         self.encoder = CNNEncoder(lambda_value)
         self.decoder = CNNDecoder(lambda_value)
@@ -14,10 +13,9 @@ class CNNAutoencoder(nn.Module):
         reconstructed_data = self.decoder(latent_features)
         return reconstructed_data
 
-
 class CNNEncoder(nn.Module):
     def __init__(self, lambda_value):
-        super(CNNEncoder, self).__init__()
+        super().__init__()
         self.lambda_value = lambda_value
 
         # Encoder layers
@@ -29,13 +27,20 @@ class CNNEncoder(nn.Module):
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2)  # 8x14x14 -> 8x7x7
         )
-        self.fc1 = nn.Linear(8 * 7 * 7, 392)  # Linear layer with 392 output units
-        self.fc2 = nn.Linear(392, lambda_value)  # Linear layer with lambda units
+        self.fc1 = nn.Sequential(
+            nn.Linear(8 * 7 * 7, 392),
+            nn.ReLU()
+        )  # Linear layer with 392 output units
+        self.fc2 = nn.Sequential(
+            nn.Linear(392, lambda_value),
+            nn.ReLU()
+        )  # Linear layer with lambda units
 
     def forward(self, x):
         x = self.encoder(x)
         x = x.view(x.size(0), -1)  # Flatten the tensor
-        latent_features = self.fc2(self.fc1(x))  # Linear layers
+        x = self.fc1(x)
+        latent_features = self.fc2(x)  # Linear layers
         return latent_features
 
 
@@ -44,18 +49,33 @@ class CNNDecoder(nn.Module):
         super(CNNDecoder, self).__init__()
         self.lambda_value = lambda_value
 
-        # Decoder layers
-        self.fc3 = nn.Linear(lambda_value, 392)  # Linear layer with 392 input units
-        self.fc4 = nn.Linear(392, 8 * 7 * 7)  # Linear layer to match the flattened shape
+        # Define the decoder layers using nn.Sequential
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(8, 16, kernel_size=3, stride=2, padding=1, output_padding=1),  # 8x7x7 -> 16x14x14
+            # Linear layer: Lambda -> 392
+            nn.Linear(self.lambda_value, 392),
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 1, kernel_size=3, stride=2, padding=1, output_padding=1),  # 16x14x14 -> 1x28x28
-            nn.ReLU()
+
+            # Reshape to 8x7x7
+            nn.Unflatten(1, (8, 7, 7)),
+
+            # ConvTranspose layers
+            nn.ConvTranspose2d(8, 8, kernel_size=4, stride=2, padding=1),  # 8x7x7 -> 8x14x14
+            nn.ReLU(),
+            nn.ConvTranspose2d(8, 16, kernel_size=3, stride=1, padding=1),  # 8x14x14 -> 16x14x14
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 16, kernel_size=4, stride=2, padding=1),  # 16x14x14 -> 16x28x28
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, kernel_size=3, stride=1, padding=1),  # 16x28x28 -> 1x28x28
+            # nn.ReLU()  # Apply relu
         )
 
     def forward(self, x):
-        x = self.fc4(self.fc3(x))  # Linear layers
-        x = x.view(x.size(0), 8, 7, 7)  # Reshape to match the decoder input
-        reconstructed_image = self.decoder(x)
-        return reconstructed_image
+        return self.decoder(x)
+
+
+if __name__ == '__main__':
+    encoder = CNNEncoder(2)
+    summary(encoder, (1,28,28), batch_dim = 0, col_names = ["input_size", "output_size", "num_params", "kernel_size", "mult_adds"], verbose = 1)
+    decoder = CNNDecoder(2)
+    summary(decoder, (2,), batch_dim = 0, col_names = ["input_size", "output_size", "num_params", "kernel_size", "mult_adds"], verbose = 1)
+
