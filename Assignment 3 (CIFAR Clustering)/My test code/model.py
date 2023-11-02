a1c63caccb3b3f1345bc278d7e50d3e05714085f
Parashara <parasharar@gmail.com>
2023-11-02 11:36:12 +0800

    now have to try it out in colab!

--- Assignment 3 (CIFAR Clustering)/My test code/model.py
+++ Assignment 3 (CIFAR Clustering)/My test code/model.py
@@ -82,15 +82,6 @@ class WideResidualBlocks(nn.Module):
         return self.blocks(x)
 
 
-class Reshape(nn.Module):
-    def __init__(self, *target_shape):
-        super(Reshape, self).__init__()
-        self.target_shape = target_shape
-
-    def forward(self, x):
-        return x.view(x.size(0), *self.target_shape)
-
-
 class Autoencoder(nn.Module):
     def __init__(self):
         super().__init__()
